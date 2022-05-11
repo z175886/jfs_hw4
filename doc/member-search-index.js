2c1221b73885e545234660dd6ed6d1c061eff626
z175886 <z295888729@gmail.com>
2022-05-11 17:09:43 -0400

    upload javadoc

---
+++ doc/member-search-index.js
@@ -0,0 +1,1 @@
+memberSearchIndex = [{"p":"<Unnamed>","c":"LinkedListTest","l":"LinkedListTest()","u":"%3Cinit%3E()"},{"p":"<Unnamed>","c":"LinkedListTest","l":"main(String[])","u":"main(java.lang.String[])"}];updateSearchResults();
